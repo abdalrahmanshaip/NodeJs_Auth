8c0f1bbc9d8af7df3dc35ff5535d77178b35d12c
abdalrahman <abdalrahmanshaip@gmail.com>
2025-02-24 20:05:28 +0200

    - Create Forgot password & reset passsord and changePassword

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -4,6 +4,8 @@ import jwt from 'jsonwebtoken'
 import { jwtGeneration } from '../utils/jwtgenration.js'
 import { validationResult } from 'express-validator'
 import { verifyRefresh } from '../middleware/authMiddleware.js'
+import crypto from 'crypto'
+import nodemailer from 'nodemailer'
 
 export const getUser = async (req, res) => {
   try {
@@ -83,6 +85,105 @@ export const login = async (req, res) => {
     })
   } catch (error) {
     return res.status(500).json({ status: 500, message: error.message })
+  }
+}
+
+export const forgotPass = async (req, res) => {
+  const { email } = req.body
+  try {
+    const user = await User.findOne({ email })
+    if (!user) {
+      return res.status(404).json({ status: 404, message: 'User not found' })
+    }
+
+    const token = crypto.randomBytes(20).toString('hex')
+    user.resetPasswordToken = token
+    user.resetPasswordExpires = Date.now() + 3600000 // 1 hour
+
+    await user.save()
+
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASS,
+      },
+    })
+
+    const mailOptions = {
+      to: user.email,
+      from: 'passwordreset@demo.com',
+      subject: 'Password Reset',
+      text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
+              Please click on the following link, or paste this into your browser to complete the process:\n\n
+              http://${req.headers.host}/reset/${token}\n\n
+              If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+    }
+
+    await transporter.sendMail(mailOptions)
+
+    return res.status(200).json({ status: 200, message: 'Email sent' })
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: error.message })
+  }
+}
+
+export const resetPass = async (req, res) => {
+  const { password } = req.body
+  const { token } = req.params
+  try {
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpires: { $gt: Date.now() },
+    })
+    if (!user) {
+      return res.status(400).json({
+        status: 400,
+        message: 'Password reset token is invalid or has expired',
+      })
+    }
+
+    user.password = await bcrypt.hash(password, 10)
+    user.resetPasswordToken = undefined
+    user.resetPasswordExpires = undefined
+
+    await user.save()
+
+    return res
+      .status(200)
+      .json({ status: 200, message: 'Password has been reset' })
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: error.message })
+  }
+}
+
+export const changePass = async (req, res) => {
+  const { oldPassword, newPassword } = req.body
+  if (!oldPassword || !newPassword) {
+    return res.status(400).json({
+      success: false,
+      error: 'oldPassword and newPassword is required',
+    })
+  }
+  const email = req.email
+  try {
+    const user = await User.findOne({ email })
+    const isMatch = await bcrypt.compare(oldPassword, user.password)
+
+    if (!isMatch) {
+      return res.status(400).json({ success: false, error: 'Invalid password' })
+    }
+
+    const newhashPassword = await bcrypt.hash(newPassword, 10)
+    user.password = newhashPassword
+    await user.save()
+    return res
+      .status(200)
+      .json({ success: true, message: 'Password changed successfully' })
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ success: false, error: 'Invalid token, try login again' })
   }
 }
 
--- src/module/userModule.js
+++ src/module/userModule.js
@@ -25,6 +25,12 @@ const userSchema = new Schema(
       type: String,
       enum: ['user', 'admin'],
     },
+    resetPasswordToken: {
+      type: String,
+    },
+    resetPasswordExpires: {
+      type: Date,
+    },
   },
   { timeseries: true }
 )
