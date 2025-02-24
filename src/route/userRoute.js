e608059a34fca1d46688ec9d67368f66530494d7
abdalrahman <abdalrahmanshaip@gmail.com>
2025-02-24 20:05:28 +0200

    - Create Forgot password & reset passsord and changePassword

--- src/route/userRoute.js
+++ src/route/userRoute.js
@@ -5,6 +5,9 @@ import {
   register,
   refreshToken,
   logout,
+  forgotPass,
+  resetPass,
+  changePass
 } from '../controller/userController.js'
 import { verifyToken } from '../middleware/authMiddleware.js'
 import { userValidation } from '../utils/authValidation.js'
@@ -14,8 +17,10 @@ router.route('/register').post(userValidation(), register)
 router.route('/login').post(login)
 router.route('/refresh').post(refreshToken)
 router.route('/logout').get(logout)
+router.route('/forgot-pass').post(forgotPass)
 router.route('/').get(verifyToken, getUser)
 
-router.route('/:id').get().put().delete()
+router.route('/reset/:token').post(resetPass)
+router.route('/change-pass').post(verifyToken, changePass)
 
 export default router
