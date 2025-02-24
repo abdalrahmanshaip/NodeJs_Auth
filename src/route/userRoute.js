e6009507dc97065a74f32fa390e04eb456dd56f4
abdalrahman <abdalrahmanshaip@gmail.com>
2025-02-24 18:27:41 +0200

    Create Logout route

--- src/route/userRoute.js
+++ src/route/userRoute.js
@@ -1,5 +1,11 @@
 import { Router } from 'express'
-import { getUser, login, register, refreshToken } from '../controller/userController.js'
+import {
+  getUser,
+  login,
+  register,
+  refreshToken,
+  logout,
+} from '../controller/userController.js'
 import { verifyToken } from '../middleware/authMiddleware.js'
 import { userValidation } from '../utils/authValidation.js'
 const router = Router()
@@ -7,6 +13,7 @@ const router = Router()
 router.route('/register').post(userValidation(), register)
 router.route('/login').post(login)
 router.route('/refresh').post(refreshToken)
+router.route('/logout').get(logout)
 router.route('/').get(verifyToken, getUser)
 
 router.route('/:id').get().put().delete()
