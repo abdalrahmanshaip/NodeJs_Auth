f644f5aa18e2510255cc36fd7d828d6d6857ed3b
abdalrahman <abdalrahmanshaip@gmail.com>
2025-02-24 17:23:33 +0200

    - Create Login with accessToken and RefreshToken

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,10 +4,20 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies['accessToken']
   if (!token) return res.status(401).json({ message: 'Access Denied' })
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    req.userId = decoded.userId
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_ACCESSTOKEN)
+    req.email = decoded.email
     next()
   } catch (error) {
     res.status(401).json({ error: error.message })
+  }
+}
+export const verifyRefresh = (email, token) => {
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_REFRESHTOKEN)
+    console.log(decoded)
+    return decoded.email === email
+  } catch (error) {
+    console.error(error)
+    return false
   }
 }
--- src/route/userRoute.js
+++ src/route/userRoute.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
-import { getUser, login, register } from '../controller/userController.js'
+import { getUser, login, register, refreshToken } from '../controller/userController.js'
 import { verifyToken } from '../middleware/authMiddleware.js'
 import { userValidation } from '../utils/authValidation.js'
 const router = Router()
 
-router.route('/').get(verifyToken, getUser)
 router.route('/register').post(userValidation(), register)
 router.route('/login').post(login)
+router.route('/refresh').post(refreshToken)
+router.route('/').get(verifyToken, getUser)
 
 router.route('/:id').get().put().delete()
 
